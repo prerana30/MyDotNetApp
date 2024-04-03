c434f14ad16171efd1b0dd35aa77ed0486d2c0e3
phenol2002 <86253756+prerana30@users.noreply.github.com>
2024-04-04 05:14:19 +0545

    Added few changes for second check

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Worlds!");
+app.MapGet("/", () => "Hello Worlds let's rock!");
 
 app.Run();
